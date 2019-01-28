e18f893d42d85e7c63134da01ebb003f09860d60
arnodp <depotter.arno@gmail.com>
2019-01-28 19:33:50 +0100

    Admin_cocktailOverview_WithVue.js

---
+++ CExplorer.MVC/wwwroot/js/Admin.js
@@ -0,0 +1,38 @@
+﻿
+var apiURL = 'https://localhost:44385/api/';
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Loading',
+        cocktails: null,
+        currentCocktail: null
+    },
+    created: function () {
+        var self = this;
+        self.fetchCocktails();
+    },
+    methods: {
+        fetchCocktails: function () {
+            self = this;
+            fetch(`${apiURL}Cocktails/Basic`)
+                .then(resp => resp.json())
+                .then(function (cocktails) {
+                    cocktails.forEach(function (cocktail, i) {
+                        cocktail.isActive = false;
+                    });
+                    self.cocktails = cocktails;
+                    self.message = 'Overview';
+                    if (self.cocktails.lenght > 0) {
+                        self.message = 'no cocktails ?';
+                    }
+                })
+
+        },
+
+        getCocktailClass: function (cocktail) {
+            if (cocktail.isActive) return 'list-group-item active';
+            return 'list-group-item';
+        },
+    }
+})
